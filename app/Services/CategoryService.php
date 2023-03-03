824abab77f1aa35377dc4a864268db0f9f3f8b90
tri-suli <trisulipras@gmail.com>
2023-03-03 22:06:46 +0700

    TRX-01: Initial Commit
    - setup service class for resources
    - ignore testing env

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Services;
+
+/**
+ * Service to handle category resources
+ * 
+ * @final
+ * @author Tri Suli Prasetyo <trisulipras@gmail.com>
+ */
+final class CategoryService
+{
+    //
+}
