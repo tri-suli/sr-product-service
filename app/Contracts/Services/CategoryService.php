6fcabd7e4288537ee766755544ae1552160841fd
tri-suli <trisulipras@gmail.com>
2023-03-04 08:42:16 +0700

    TRX-03: Implements query methods in category service
    - Adding interface
    - Boot the contrac service into service container

---
+++ app/Contracts/Services/CategoryService.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace App\Contracts\Services;
+
+use App\Contracts\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
+
+interface CategoryService
+{
+}
--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,13 +2,14 @@
 
 namespace App\Services;
 
+use App\Contracts\Services\CategoryService as ServiceContract;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
 /**
  * Service to handle category resources
  * 
  * @final
  * @author Tri Suli Prasetyo <trisulipras@gmail.com>
  */
-final class CategoryService
-{
-    //
+final class CategoryService implements ServiceContract
 }
